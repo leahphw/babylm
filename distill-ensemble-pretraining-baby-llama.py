7f7f7219c7c27911f3d5c6e1c01bcdbd531bcecf
Jan Stipl <jan.stipl@richmond.edu>
2025-04-11 12:33:12 -0400

    Refactor and update paths

--- distill-ensemble-pretraining-baby-llama.py
+++ distill-ensemble-pretraining-baby-llama.py
@@ -1,3 +1,7 @@
+import os
+
+os.environ["WANDB_DISABLED"] = "true"
+
 from transformers import (
     GPT2TokenizerFast,
     LlamaForCausalLM,
@@ -10,91 +14,16 @@ from transformers import (
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import  Subset
-from random import sample
+from torch.utils.data import Subset
+import random
 
 from pathlib import Path
+
 # import wandb
 
 
 from babylm_dataset import BabylmDataset
-
-
-#############
-LR = 2.5e-4
-BATCH_SIZE = 32
-SEQ_LENGTH = 128
-
-TEMPERATURE = 2.0
-ALPHA = 0.5
-#############
-
-
-PATH = Path("./")
-
-teacher_dir1 = PATH / 'models/Llama-16M'
-teacher_dir2 = PATH / 'models/GPT2-97M'
-
-
-MODEL_NAME = f'Baby-Llama-58M'
-MODEL_OUTPUT = Path('./models') /  MODEL_NAME
-EVAL_SAMPLES = 8192
-
-
-wandb_log = False # True when we will import wandb !!!!!
-
-
-
-tokenizer_path = "/scratch/jstipl/models/gpt-clean-16000.json"
-tokenizer = GPT2TokenizerFast(tokenizer_file= str(tokenizer_path))
-tokenizer.bos_token = "<s>"
-tokenizer.eos_token = "</s>"
-tokenizer.pad_token = "<pad>"
-
-# in the original code I had random_chunk = False
-# random_chunk=True is expected to improve the model performance a bit
-train_dataset = BabylmDataset("/scratch/jstipl/data/train_10M_clean", SEQ_LENGTH, tokenizer=tokenizer, random_chunk=True)
-full_eval_dataset = BabylmDataset("/scratch/jstipl/data/dev_clean", SEQ_LENGTH, tokenizer=tokenizer, offset=0)
-
-eval_indices = sample(range(len(full_eval_dataset)), EVAL_SAMPLES)
-eval_dataset = Subset(full_eval_dataset, eval_indices)
-
-
-
-
-tokenizer.model_max_length = SEQ_LENGTH
-
-config = LlamaConfig(
-    vocab_size=tokenizer.vocab_size,
-    hidden_size=512,
-    num_hidden_layers=16,
-    intermediate_size=1024,
-    num_attention_heads=8,
-    bos_token_id=tokenizer.convert_tokens_to_ids("<s>"),
-    eos_token_id=tokenizer.convert_tokens_to_ids("</s>"),
-    pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
-    max_position_embeddings=2*SEQ_LENGTH,
-)
-
-student = LlamaForCausalLM(config)
-# student = LlamaForCausalLM.from_pretrained(student_dir)
-
-
-teacher1 = LlamaForCausalLM.from_pretrained(teacher_dir1)
-teacher2 = GPT2LMHeadModel.from_pretrained(teacher_dir2)
-teachers = [teacher1, teacher2]
-
-
-data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer, mlm=False,
-)
-
-
-print(f'model num parameters: student = {student.num_parameters()}')
-print(f'model num parameters: teacher1 = {teacher1.num_parameters()}')
-print(f'model num parameters: teacher2 = {teacher2.num_parameters()}')
-
-
+import consts
 
 #  Distillation Trainer
 #  We modified the Trainer from this repo https://github.com/philschmid/knowledge-distillation-transformers-pytorch-sagemaker
@@ -135,87 +64,150 @@ class DistillationTrainer(Trainer):
 
         # Soften probabilities and compute distillation loss
         loss_function = nn.KLDivLoss(reduction="batchmean")
-        loss_logits = (
-            loss_function(
-                F.log_softmax(outputs_student.logits / self.args.temperature, dim=-1),
-                F.softmax(avg_teacher_logits / self.args.temperature, dim=-1),
-            )
-            * (self.args.temperature ** 2)
-        )
+        loss_logits = loss_function(
+            F.log_softmax(outputs_student.logits / self.args.temperature, dim=-1),
+            F.softmax(avg_teacher_logits / self.args.temperature, dim=-1),
+        ) * (self.args.temperature**2)
         # Return weighted student loss
         loss = self.args.alpha * student_loss + (1.0 - self.args.alpha) * loss_logits
         return (loss, outputs_student) if return_outputs else loss
 
 
-if wandb_log:
-    wandb.login()
-    wandb.init(project='babylm', name=MODEL_NAME)
+def create_student(tokenizer, seq_length):
+    config = LlamaConfig(
+        vocab_size=tokenizer.vocab_size,
+        hidden_size=512,
+        num_hidden_layers=16,
+        intermediate_size=1024,
+        num_attention_heads=8,
+        bos_token_id=tokenizer.convert_tokens_to_ids("<s>"),
+        eos_token_id=tokenizer.convert_tokens_to_ids("</s>"),
+        pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
+        max_position_embeddings=2 * seq_length,
+    )
+
+    student = LlamaForCausalLM(config)
+    return student
 
 
+def check_gpu_availability():
+
+    if torch.cuda.is_available():
+        print(f"Number of GPUs: {torch.cuda.device_count()}")
+        for i in range(torch.cuda.device_count()):
+            print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+
+        print("To use all GPUs run: torchrun --nproc_per_node=2 train.py")
+    else:
+        print("No GPU found, using CPU.")
+        print("Exiting")
+        exit(1)
+
+    assert (
+        torch.cuda.device_count() == 2
+    ), "Using too many GPUs, professor will not be happy"
 
 
+def main():
+    check_gpu_availability()
+    random.seed(consts.RANDOM_SEED)
 
-training_args = DistillationTrainingArguments(
-    output_dir=MODEL_OUTPUT,
-    overwrite_output_dir=True,
-    save_strategy = "epoch",
-    evaluation_strategy = "epoch",
-    num_train_epochs=6,
-    gradient_accumulation_steps=1,
-    per_device_train_batch_size=BATCH_SIZE,
-    save_total_limit=1,  # Set to zero to avoid saving
-    # report_to="wandb",
-    warmup_steps=200, 
-    lr_scheduler_type="cosine",
-    learning_rate=LR,
-    logging_steps=20,
-    fp16=True,
-    load_best_model_at_end=True,
-    metric_for_best_model="eval_loss",
-    weight_decay=0.1,
-    alpha=ALPHA,
-    temperature=TEMPERATURE,
-)
+    #############
+    LR = 2.5e-4
+    BATCH_SIZE = 32
+    SEQ_LENGTH = 128
 
+    TEMPERATURE = 2.0
+    ALPHA = 0.5
 
-trainer = DistillationTrainer(
+    EVAL_SAMPLES = 8192
+    #############
+
+    TEACHER_DIR1 = consts.TEACHER_DIR / "Llama-16M"
+    TEACHER_DIR2 = consts.TEACHER_DIR / "GPT2-97M"
+
+    STUDENT_NAME = "Baby-Llama-58M"
+    STUDENT_OUTPUT = consts.TEACHER_DIR / STUDENT_NAME
+
+    wandb_log = False
+
+    tokenizer = GPT2TokenizerFast(tokenizer_file=str(consts.TOKENIZER_PATH))
+    tokenizer.bos_token = "<s>"
+    tokenizer.eos_token = "</s>"
+    tokenizer.pad_token = "<pad>"
+    tokenizer.model_max_length = SEQ_LENGTH
+
+    train_dataset = BabylmDataset(
+        consts.TRAIN_DATASET_STRICT_PATH,
+        SEQ_LENGTH,
+        tokenizer=tokenizer,
+        random_chunk=True,
+    )
+    full_eval_dataset = BabylmDataset(
+        consts.DEV_DATASET_STRICT_PATH, SEQ_LENGTH, tokenizer=tokenizer, offset=0
+    )
+
+    eval_indices = random.sample(range(len(full_eval_dataset)), EVAL_SAMPLES)
+    eval_dataset = Subset(full_eval_dataset, eval_indices)
+    del full_eval_dataset
+
+    teacher1 = LlamaForCausalLM.from_pretrained(TEACHER_DIR1)
+    teacher2 = GPT2LMHeadModel.from_pretrained(TEACHER_DIR2)
+    teachers = [teacher1, teacher2]
+    # teachers = [teacher1]
+
+    student = create_student(tokenizer, SEQ_LENGTH)
+
+    print(f"model num parameters: student = {student.num_parameters()}")
+    print(f"model num parameters: teacher1 = {teacher1.num_parameters()}")
+    # print(f"model num parameters: teacher2 = {teacher2.num_parameters()}")
+
+    if wandb_log:
+        wandb.login()
+        wandb.init(project="babylm", name=STUDENT_NAME)
+
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer,
+        mlm=False,
+    )
+
+    training_args = DistillationTrainingArguments(
+        output_dir=STUDENT_OUTPUT,
+        overwrite_output_dir=True,
+        save_strategy="epoch",
+        evaluation_strategy="epoch",
+        num_train_epochs=6,
+        gradient_accumulation_steps=1,
+        per_device_train_batch_size=BATCH_SIZE,
+        save_total_limit=1,  # Set to zero to avoid saving
+        report_to=[],  # "wandb",
+        warmup_steps=200,
+        lr_scheduler_type="cosine",
+        learning_rate=LR,
+        logging_steps=20,
+        fp16=True,
+        load_best_model_at_end=True,
+        metric_for_best_model="eval_loss",
+        weight_decay=0.1,
+        alpha=ALPHA,
+        temperature=TEMPERATURE,
+    )
+
+    trainer = DistillationTrainer(
         student,
         training_args,
         teacher_models=teachers,
         data_collator=data_collator,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
-
     )
 
-if torch.cuda.is_available():
-    print(f"Number of GPUs: {torch.cuda.device_count()}")
-    for i in range(torch.cuda.device_count()):
-        print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
-        
-    print("To use all GPUs run: torchrun --nproc_per_node=2 train.py")
-else:
-    print("No GPU found, using CPU.")
-    print("Exiting")
-    exit(1)
+    print(f"Trainer is using device: {trainer.args.device}")
 
-assert torch.cuda.device_count() == 2, "Using too many GPUs, professor will not be happy"
+    trainer.train()
+    trainer.save_model(STUDENT_OUTPUT)
+    tokenizer.save_pretrained(STUDENT_OUTPUT)
 
 
-print(f"Trainer is using device: {trainer.args.device}")
-trainer.train()
-
-
-trainer.save_model(MODEL_OUTPUT)
-tokenizer.save_pretrained(MODEL_OUTPUT)
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
