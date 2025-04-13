e51ea7ad82796ad6037139ae0115b813656e52a6
pl3nt <leah.le@richmond.edu>
2025-04-12 23:01:29 -0400

    separate distillation experiements and restore train script

--- distill-ensemble-pretraining-baby-llama.py
+++ distill-ensemble-pretraining-baby-llama.py
@@ -1,7 +1,3 @@
-import os
-
-os.environ["WANDB_DISABLED"] = "true"
-
 from transformers import (
     GPT2TokenizerFast,
     LlamaForCausalLM,
@@ -14,17 +10,91 @@ from transformers import (
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import Subset
-import random
+from torch.utils.data import  Subset
+from random import sample
 
 from pathlib import Path
-
 # import wandb
 
 
 from babylm_dataset import BabylmDataset
-import consts
-from datetime import datetime
+
+
+#############
+LR = 2.5e-4
+BATCH_SIZE = 32
+SEQ_LENGTH = 128
+
+TEMPERATURE = 2.0
+ALPHA = 0.5
+#############
+
+
+PATH = Path("./")
+
+teacher_dir1 = PATH / 'models/Llama-16M'
+teacher_dir2 = PATH / 'models/GPT2-97M'
+
+
+MODEL_NAME = f'Baby-Llama-58M'
+MODEL_OUTPUT = Path('./models') /  MODEL_NAME
+EVAL_SAMPLES = 8192
+
+
+wandb_log = False # True when we will import wandb !!!!!
+
+
+
+tokenizer_path = "/scratch/jstipl/models/gpt-clean-16000.json"
+tokenizer = GPT2TokenizerFast(tokenizer_file= str(tokenizer_path))
+tokenizer.bos_token = "<s>"
+tokenizer.eos_token = "</s>"
+tokenizer.pad_token = "<pad>"
+
+# in the original code I had random_chunk = False
+# random_chunk=True is expected to improve the model performance a bit
+train_dataset = BabylmDataset("/scratch/jstipl/data/train_10M_clean", SEQ_LENGTH, tokenizer=tokenizer, random_chunk=True)
+full_eval_dataset = BabylmDataset("/scratch/jstipl/data/dev_clean", SEQ_LENGTH, tokenizer=tokenizer, offset=0)
+
+eval_indices = sample(range(len(full_eval_dataset)), EVAL_SAMPLES)
+eval_dataset = Subset(full_eval_dataset, eval_indices)
+
+
+
+
+tokenizer.model_max_length = SEQ_LENGTH
+
+config = LlamaConfig(
+    vocab_size=tokenizer.vocab_size,
+    hidden_size=512,
+    num_hidden_layers=16,
+    intermediate_size=1024,
+    num_attention_heads=8,
+    bos_token_id=tokenizer.convert_tokens_to_ids("<s>"),
+    eos_token_id=tokenizer.convert_tokens_to_ids("</s>"),
+    pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
+    max_position_embeddings=2*SEQ_LENGTH,
+)
+
+student = LlamaForCausalLM(config)
+# student = LlamaForCausalLM.from_pretrained(student_dir)
+
+
+teacher1 = LlamaForCausalLM.from_pretrained(teacher_dir1)
+teacher2 = GPT2LMHeadModel.from_pretrained(teacher_dir2)
+teachers = [teacher1, teacher2]
+
+
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, mlm=False,
+)
+
+
+print(f'model num parameters: student = {student.num_parameters()}')
+print(f'model num parameters: teacher1 = {teacher1.num_parameters()}')
+print(f'model num parameters: teacher2 = {teacher2.num_parameters()}')
+
+
 
 #  Distillation Trainer
 #  We modified the Trainer from this repo https://github.com/philschmid/knowledge-distillation-transformers-pytorch-sagemaker
@@ -32,269 +102,109 @@ from datetime import datetime
 
 
 class DistillationTrainingArguments(TrainingArguments):
-    def __init__(self, *args, alpha=0.5, temperature=2.0, contrastive_weight=0.1, **kwargs):
+    def __init__(self, *args, alpha=0.5, temperature=2.0, **kwargs):
         super().__init__(*args, **kwargs)
         self.alpha = alpha
         self.temperature = temperature
-        self.contrastive_weight = contrastive_weight
 
 
-class ContrastiveDistillationTrainer(Trainer):
-    """Trainer implementing contrastive distillation with ensemble of teachers."""
-    
+class DistillationTrainer(Trainer):
     def __init__(self, *args, teacher_models=None, **kwargs):
-        """
-        Args:
-            teacher_models (list): List of teacher models to distill from
-        """
         super().__init__(*args, **kwargs)
         self.teachers = teacher_models
         for teacher in self.teachers:
+            # place each teacher on same device as student
             self._move_model_to_device(teacher, self.model.device)
             teacher.eval()
-        
-        # Projection heads for contrastive learning
-        # Map all hidden states to a common dimension (256)
-        self.student_projection = nn.Linear(512, 256).to(self.model.device)
-        self.teacher_projections = nn.ModuleList([
-            nn.Linear(teacher.config.hidden_size, 256).to(self.model.device)
-            for teacher in self.teachers
-        ])
-        
-    def compute_contrastive_loss(self, student_hidden, teacher_hidden):
-        """
-        Compute contrastive loss between student and teacher hidden states.
-        
-        Args:
-            student_hidden (torch.Tensor): Student model hidden states [batch_size, seq_length, hidden_dim]
-            teacher_hidden (torch.Tensor): Teacher model hidden states [batch_size, seq_length, hidden_dim]
-            
-        Returns:
-            torch.Tensor: Contrastive loss
-        """
-        batch_size, seq_length, hidden_dim = student_hidden.shape
-        
-        # Project and normalize hidden states
-        student_proj = F.normalize(self.student_projection(student_hidden), dim=-1)  # [batch_size, seq_length, proj_dim]
-        teacher_proj = F.normalize(teacher_hidden, dim=-1)  # [batch_size, seq_length, proj_dim]
-        
-        # Reshape to combine batch and sequence dimensions
-        student_proj = student_proj.reshape(-1, student_proj.size(-1))  # [batch_size * seq_length, proj_dim]
-        teacher_proj = teacher_proj.reshape(-1, teacher_proj.size(-1))  # [batch_size * seq_length, proj_dim]
-        
-        # Compute similarity matrix with temperature scaling
-        similarity = torch.matmul(student_proj, teacher_proj.transpose(-2, -1)) / self.args.temperature
-        
-        # Create labels (diagonal matrix)
-        labels = torch.arange(similarity.size(0), device=similarity.device)
-        
-        return F.cross_entropy(similarity, labels)
 
     def compute_loss(self, model, inputs, return_outputs=False):
-        """
-        Compute combined loss for contrastive distillation.
-        
-        Args:
-            model: Student model
-            inputs: Input tensors
-            return_outputs: Whether to return model outputs
-            
-        Returns:
-            torch.Tensor or tuple: Loss or (loss, outputs)
-        """
-        # Get student outputs with hidden states
-        outputs_student = model(**inputs, output_hidden_states=True)
+        # compute student output
+        outputs_student = model(**inputs)
         student_loss = outputs_student.loss
-        
-        # Get teacher outputs with hidden states
+
+        # compute teacher output
         with torch.no_grad():
             all_teacher_logits = []
-            all_teacher_hidden = []
-            for i, teacher in enumerate(self.teachers):
-                outputs_teacher = teacher(**inputs, output_hidden_states=True)
+            for teacher in self.teachers:
+                outputs_teacher = teacher(**inputs)
                 all_teacher_logits.append(outputs_teacher.logits)
-                # Project teacher hidden states to common dimension
-                projected_hidden = self.teacher_projections[i](outputs_teacher.hidden_states[-1])
-                all_teacher_hidden.append(projected_hidden)
-            
             avg_teacher_logits = torch.stack(all_teacher_logits).mean(dim=0)
-            avg_teacher_hidden = torch.stack(all_teacher_hidden).mean(dim=0)
 
-        # Standard distillation loss
+        # assert size
+        assert outputs_student.logits.size() == avg_teacher_logits.size()
+
+        # Soften probabilities and compute distillation loss
         loss_function = nn.KLDivLoss(reduction="batchmean")
-        loss_logits = loss_function(
-            F.log_softmax(outputs_student.logits / self.args.temperature, dim=-1),
-            F.softmax(avg_teacher_logits / self.args.temperature, dim=-1),
-        ) * (self.args.temperature**2)
-        
-        # Contrastive loss on hidden states
-        contrastive_loss = self.compute_contrastive_loss(
-            outputs_student.hidden_states[-1],
-            avg_teacher_hidden
+        loss_logits = (
+            loss_function(
+                F.log_softmax(outputs_student.logits / self.args.temperature, dim=-1),
+                F.softmax(avg_teacher_logits / self.args.temperature, dim=-1),
+            )
+            * (self.args.temperature ** 2)
         )
-        
-        # Combined loss
-        loss = (
-            self.args.alpha * student_loss + 
-            (1.0 - self.args.alpha) * loss_logits +
-            self.args.contrastive_weight * contrastive_loss
-        )
-        
+        # Return weighted student loss
+        loss = self.args.alpha * student_loss + (1.0 - self.args.alpha) * loss_logits
         return (loss, outputs_student) if return_outputs else loss
 
 
-def create_student(tokenizer, seq_length):
-    config = LlamaConfig(
-        vocab_size=tokenizer.vocab_size,
-        hidden_size=512,
-        num_hidden_layers=16,
-        intermediate_size=1024,
-        num_attention_heads=8,
-        bos_token_id=tokenizer.convert_tokens_to_ids("<s>"),
-        eos_token_id=tokenizer.convert_tokens_to_ids("</s>"),
-        pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
-        max_position_embeddings=2 * seq_length,
-    )
-
-    student = LlamaForCausalLM(config)
-    return student
+if wandb_log:
+    wandb.login()
+    wandb.init(project='babylm', name=MODEL_NAME)
 
 
-def check_gpu_availability():
-
-    if torch.cuda.is_available():
-        print(f"Number of GPUs: {torch.cuda.device_count()}")
-        for i in range(torch.cuda.device_count()):
-            print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
-
-        print("To use all GPUs run: torchrun --nproc_per_node=2 train.py")
-    else:
-        print("No GPU found, using CPU.")
-        print("Exiting")
-        exit(1)
-
-    assert (
-        torch.cuda.device_count() == 2
-    ), "Using too many GPUs, professor will not be happy"
 
 
-def main():
-    check_gpu_availability()
-    random.seed(consts.RANDOM_SEED)
 
-    #############
-    LR = 2.5e-4
-    BATCH_SIZE = 32
-    SEQ_LENGTH = 128
+training_args = DistillationTrainingArguments(
+    output_dir=MODEL_OUTPUT,
+    overwrite_output_dir=True,
+    save_strategy = "epoch",
+    evaluation_strategy = "epoch",
+    num_train_epochs=6,
+    gradient_accumulation_steps=1,
+    per_device_train_batch_size=BATCH_SIZE,
+    save_total_limit=1,  # Set to zero to avoid saving
+    # report_to="wandb",
+    warmup_steps=200, 
+    lr_scheduler_type="cosine",
+    learning_rate=LR,
+    logging_steps=20,
+    fp16=True,
+    load_best_model_at_end=True,
+    metric_for_best_model="eval_loss",
+    weight_decay=0.1,
+    alpha=ALPHA,
+    temperature=TEMPERATURE,
+)
 
-    TEMPERATURE = 2.0
-    ALPHA = 0.5
-    CONTRASTIVE_WEIGHT = 0.1  # Weight for contrastive loss
 
-    EVAL_SAMPLES = 8192
-    #############
-
-    TEACHER_DIR1 = consts.TEACHER_DIR / "Llama-16M"
-    TEACHER_DIR2 = Path("/scratch/nlp_G1/teachers/GPT2-97M")
-
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    STUDENT_NAME = "Baby-Llama-58M"
-    STUDENT_OUTPUT = consts.TEACHER_DIR / f"{STUDENT_NAME}_{timestamp}"
-
-    wandb_log = False
-
-    tokenizer = GPT2TokenizerFast(tokenizer_file=str(consts.TOKENIZER_PATH))
-    tokenizer.bos_token = "<s>"
-    tokenizer.eos_token = "</s>"
-    tokenizer.pad_token = "<pad>"
-    tokenizer.model_max_length = SEQ_LENGTH
-
-    train_dataset = BabylmDataset(
-        consts.TRAIN_DATASET_STRICT_PATH,
-        SEQ_LENGTH,
-        tokenizer=tokenizer,
-        random_chunk=True,
-    )
-    full_eval_dataset = BabylmDataset(
-        consts.DEV_DATASET_STRICT_PATH, SEQ_LENGTH, tokenizer=tokenizer, offset=0
-    )
-
-    eval_indices = random.sample(range(len(full_eval_dataset)), EVAL_SAMPLES)
-    eval_dataset = Subset(full_eval_dataset, eval_indices)
-    del full_eval_dataset
-
-    # Load teachers with error handling
-    teachers = []
-    try:
-        teacher1 = LlamaForCausalLM.from_pretrained(TEACHER_DIR1)
-        teachers.append(teacher1)
-        print(f"Successfully loaded teacher1 from {TEACHER_DIR1}")
-    except Exception as e:
-        print(f"Failed to load teacher1: {e}")
-    
-    try:
-        teacher2 = GPT2LMHeadModel.from_pretrained(TEACHER_DIR2)
-        teachers.append(teacher2)
-        print(f"Successfully loaded teacher2 from {TEACHER_DIR2}")
-    except Exception as e:
-        print(f"Failed to load teacher2: {e}")
-
-    if not teachers:
-        raise RuntimeError("No teacher models could be loaded. Please check the model directories.")
-
-    student = create_student(tokenizer, SEQ_LENGTH)
-
-    print(f"model num parameters: student = {student.num_parameters()}")
-    for i, teacher in enumerate(teachers):
-        print(f"model num parameters: teacher{i+1} = {teacher.num_parameters()}")
-
-    if wandb_log:
-        wandb.login()
-        wandb.init(project="babylm", name=STUDENT_NAME)
-
-    data_collator = DataCollatorForLanguageModeling(
-        tokenizer=tokenizer,
-        mlm=False,
-    )
-
-    training_args = DistillationTrainingArguments(
-        output_dir=STUDENT_OUTPUT,
-        overwrite_output_dir=True,
-        save_strategy="epoch",
-        evaluation_strategy="epoch",
-        num_train_epochs=6,
-        gradient_accumulation_steps=1,
-        per_device_train_batch_size=BATCH_SIZE,
-        save_total_limit=1,
-        report_to=[],
-        warmup_steps=200,
-        lr_scheduler_type="cosine",
-        learning_rate=LR,
-        logging_steps=20,
-        fp16=True,
-        load_best_model_at_end=True,
-        metric_for_best_model="eval_loss",
-        weight_decay=0.1,
-        alpha=ALPHA,
-        temperature=TEMPERATURE,
-        contrastive_weight=CONTRASTIVE_WEIGHT,
-    )
-
-    trainer = ContrastiveDistillationTrainer(
+trainer = DistillationTrainer(
         student,
         training_args,
         teacher_models=teachers,
         data_collator=data_collator,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
+
     )
 
-    print(f"Trainer is using device: {trainer.args.device}")
+if torch.cuda.is_available():
+    print(f"Number of GPUs: {torch.cuda.device_count()}")
+    for i in range(torch.cuda.device_count()):
+        print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+        
+    print("To use all GPUs run: torchrun --nproc_per_node=2 train.py")
+else:
+    print("No GPU found, using CPU.")
+    print("Exiting")
+    exit(1)
 
-    trainer.train()
-    trainer.save_model(STUDENT_OUTPUT)
-    tokenizer.save_pretrained(STUDENT_OUTPUT)
+assert torch.cuda.device_count() == 2, "Using too many GPUs, professor will not be happy"
 
 
-if __name__ == "__main__":
-    main()
+print(f"Trainer is using device: {trainer.args.device}")
+trainer.train()
+
+
+trainer.save_model(MODEL_OUTPUT)
+tokenizer.save_pretrained(MODEL_OUTPUT)
--- train.py
+++ train.py
@@ -1,132 +1,161 @@
-"""
-Language Model Training Script
-
-This script trains a language model (GPT2, Llama, or GPTJ) on the BabyLM dataset.
-It supports configuration via YAML files and command-line arguments.
-
-Usage:
-    python train.py --config ./config/gpt-97M.yaml --lr 2.5e-4 --model_name my_model
-"""
-
 import os
-import argparse
-from pathlib import Path
-from random import sample, seed
-import yaml
-
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
 import torch
-from torch.utils.data import Subset
 from transformers import (
     GPT2Config, GPT2LMHeadModel, 
     LlamaConfig, LlamaForCausalLM, 
-    GPTJConfig, GPTJForCausalLM,
-    GPT2TokenizerFast,
-    Trainer, TrainingArguments, 
-    DataCollatorForLanguageModeling
+    GPTJConfig, GPTJForCausalLM
 )
+from transformers import Trainer, TrainingArguments, DataCollatorForLanguageModeling
+from transformers import GPT2TokenizerFast
+from torch.utils.data import Subset
+from random import sample, seed
+from pathlib import Path
+import yaml
+import argparse
 
 from babylm_dataset import BabylmDataset
 
-# Set CUDA devices
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
 
 
-def parse_args():
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser(description="Train a language model on BabyLM dataset")
-    parser.add_argument(
-        "--config", 
-        type=str, 
-        default="./config/gpt-97M.yaml",
-        help="Configuration file path"
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", type=str, default="./config/gpt-97M.yaml", help="Configuration file path")
+parser.add_argument("--lr", type=float, default=None, help="Learning rate")
+parser.add_argument("--model_name", type=str, default=None, help="Model name")
+args = parser.parse_args()
+
+with open(args.config, 'r') as f:
+    config = yaml.safe_load(f)
+
+
+# Override config parameters if provided as command-line arguments
+if args.lr:
+    config['training']['lr'] = args.lr
+if args.model_name:
+    config['model']['name'] = args.model_name
+
+
+SEQ_LENGTH = config['data']['seq_length']
+
+tokenizer_path = config['data']['tokenizer_path']
+tokenizer = GPT2TokenizerFast(tokenizer_file= str(tokenizer_path))
+tokenizer.bos_token = "<s>"
+tokenizer.eos_token = "</s>"
+tokenizer.pad_token = "<pad>"
+
+# in the original code I had random_chunk = False
+# random_chunk=True is expected to improve the model performance a bit
+train_dataset = BabylmDataset(config['data']['train_path'], SEQ_LENGTH, tokenizer=tokenizer, random_chunk=True)
+full_eval_dataset = BabylmDataset(config['data']['eval_path'], SEQ_LENGTH, tokenizer=tokenizer, offset=0)
+
+seed(2023) # we fix the same subset for all models
+eval_indices = sample(range(len(full_eval_dataset)), config['data']['eval_samples'])
+eval_dataset = Subset(full_eval_dataset, eval_indices)
+
+# We tokenize the whole dataset and then set the max length
+tokenizer.model_max_length = SEQ_LENGTH
+
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, mlm=False,
+)
+
+# Dynamic Model Configuration
+if config['model']['type'] == "Llama":
+    model_config = LlamaConfig(
+        vocab_size=tokenizer.vocab_size,
+        max_position_embeddings=2*tokenizer.model_max_length,
+        hidden_size=config['model']['hidden_size'],
+        intermediate_size=config['model']['intermediate_size'],
+        num_hidden_layers=config['model']['n_layer'],
+        num_attention_heads=config['model']['n_head'],
+        tie_word_embeddings=config['model'].get('tie_word_embeddings', False),
+        pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
     )
-    parser.add_argument(
-        "--lr", 
-        type=float, 
-        default=None,
-        help="Learning rate (overrides config)"
+    model = LlamaForCausalLM(model_config)
+elif config['model']['type'] == "GPT2":
+    model_config = GPT2Config(
+        vocab_size=tokenizer.vocab_size,
+        n_positions=2*tokenizer.model_max_length,
+        n_embd=config['model']['hidden_size'],
+        n_layer=config['model']['n_layer'],
+        n_head=config['model']['n_head'],
+        resid_pdrop = config['model']['resid_pdrop'],
+        embd_pdrop = config['model']['embd_pdrop'],
+        attn_pdrop = config['model']['attn_pdrop'],
+        pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
     )
-    parser.add_argument(
-        "--model_name", 
-        type=str, 
-        default=None,
-        help="Model name (overrides config)"
+    model = GPT2LMHeadModel(model_config)
+elif config['model']['type'] == "GPTJ":
+    model_config = GPTJConfig(
+        vocab_size=tokenizer.vocab_size,
+        n_positions=2*tokenizer.model_max_length,
+        n_embd=config['model']['hidden_size'],
+        n_layer=config['model']['n_layer'],
+        n_head=config['model']['n_head'],
+        resid_pdrop = config['model']['resid_pdrop'],
+        embd_pdrop = config['model']['embd_pdrop'],
+        attn_pdrop = config['model']['attn_pdrop'],
+        tie_word_embeddings=config['model']['tie_word_embeddings'],
+        pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
     )
-    return parser.parse_args()
+    model = GPTJForCausalLM(model_config)
+
+print(f'model parameters = {model.num_parameters()}')
 
 
-def load_config(config_path, args):
-    """Load and update configuration from YAML file."""
-    with open(config_path, 'r') as f:
-        config = yaml.safe_load(f)
-    
-    # Override config parameters if provided as command-line arguments
-    if args.lr:
-        config['training']['lr'] = args.lr
-    if args.model_name:
-        config['model']['name'] = args.model_name
-    
-    return config
+output_dir = Path(config['logging']['output_dir']) / config['model']['name']
+accumulation_steps = config['training']['gradient_accumulation_steps']
+per_device_bsz = config['training']['batch_size'] // accumulation_steps
+
+training_args = TrainingArguments(
+    output_dir=output_dir,
+    overwrite_output_dir=True,
+    save_strategy = "epoch",
+    evaluation_strategy = "epoch",
+    num_train_epochs=config['training']['num_epochs'],
+    gradient_accumulation_steps=accumulation_steps,
+    per_device_train_batch_size=per_device_bsz,
+    save_total_limit=1,  # Set to zero to avoid saving
+    warmup_steps=config['training']['warmup_steps'], 
+    lr_scheduler_type="cosine",
+    learning_rate=float(config['training']['lr']),
+    logging_steps=20,
+    fp16=config['training']['fp16'],
+    load_best_model_at_end=True,
+    metric_for_best_model="eval_loss",
+    torch_compile = config['training'].get('torch_compile', False),
+)
+
+# Enable multi-GPU
+# if torch.cuda.device_count() > 1:
+#     print(f"Using {torch.cuda.device_count()} GPUs")
+#     model = torch.nn.DataParallel(model)
+# Use: torchrun --nproc_per_node=2 train.py
+# This uses the 2 GPUs fully
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+)
 
 
-def create_model(config, tokenizer):
-    """Create model based on configuration."""
-    model_type = config['model']['type']
-    
-    if model_type == "Llama":
-        model_config = LlamaConfig(
-            vocab_size=tokenizer.vocab_size,
-            max_position_embeddings=2*tokenizer.model_max_length,
-            hidden_size=config['model']['hidden_size'],
-            intermediate_size=config['model']['intermediate_size'],
-            num_hidden_layers=config['model']['n_layer'],
-            num_attention_heads=config['model']['n_head'],
-            tie_word_embeddings=config['model'].get('tie_word_embeddings', False),
-            pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
-        )
-        model = LlamaForCausalLM(model_config)
-    
-    elif model_type == "GPT2":
-        model_config = GPT2Config(
-            vocab_size=tokenizer.vocab_size,
-            n_positions=2*tokenizer.model_max_length,
-            n_embd=config['model']['hidden_size'],
-            n_layer=config['model']['n_layer'],
-            n_head=config['model']['n_head'],
-            resid_pdrop=config['model']['resid_pdrop'],
-            embd_pdrop=config['model']['embd_pdrop'],
-            attn_pdrop=config['model']['attn_pdrop'],
-            pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
-        )
-        model = GPT2LMHeadModel(model_config)
-    
-    elif model_type == "GPTJ":
-        model_config = GPTJConfig(
-            vocab_size=tokenizer.vocab_size,
-            n_positions=2*tokenizer.model_max_length,
-            n_embd=config['model']['hidden_size'],
-            n_layer=config['model']['n_layer'],
-            n_head=config['model']['n_head'],
-            resid_pdrop=config['model']['resid_pdrop'],
-            embd_pdrop=config['model']['embd_pdrop'],
-            attn_pdrop=config['model']['attn_pdrop'],
-            tie_word_embeddings=config['model']['tie_word_embeddings'],
-            pad_token_id=tokenizer.convert_tokens_to_ids("<pad>"),
-        )
-        model = GPTJForCausalLM(model_config)
-    
-    else:
-        raise ValueError(f"Unsupported model type: {model_type}")
-    
-    return model
+if __name__ == "__main__":
+    # This is not installed in (base)
+    # if config['logging']['wandb']:
+    #     import wandb
+    #     wandb.login()
+    #     wandb.init(project= config['logging']['project'], name=config['model']['name'], config=config)
 
 
-def check_gpu_availability():
-    """Check GPU availability and configuration."""
+    
     if torch.cuda.is_available():
         print(f"Number of GPUs: {torch.cuda.device_count()}")
         for i in range(torch.cuda.device_count()):
             print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+        
         print("To use all GPUs run: torchrun --nproc_per_node=2 train.py")
     else:
         print("No GPU found, using CPU.")
@@ -136,94 +165,7 @@ def check_gpu_availability():
     assert torch.cuda.device_count() == 2, "Using too many GPUs, professor will not be happy"
 
 
-def main():
-    """Main training function."""
-    # Parse arguments and load config
-    args = parse_args()
-    config = load_config(args.config, args)
-    
-    # Set random seed for reproducibility
-    seed(2023)
-    
-    # Initialize tokenizer
-    tokenizer = GPT2TokenizerFast(tokenizer_file=str(config['data']['tokenizer_path']))
-    tokenizer.bos_token = "<s>"
-    tokenizer.eos_token = "</s>"
-    tokenizer.pad_token = "<pad>"
-    tokenizer.model_max_length = config['data']['seq_length']
-    
-    # Prepare datasets
-    train_dataset = BabylmDataset(
-        config['data']['train_path'],
-        config['data']['seq_length'],
-        tokenizer=tokenizer,
-        random_chunk=True,  # Expected to improve model performance
-    )
-    
-    full_eval_dataset = BabylmDataset(
-        config['data']['eval_path'],
-        config['data']['seq_length'],
-        tokenizer=tokenizer,
-        offset=0
-    )
-    
-    # Create evaluation subset
-    eval_indices = sample(range(len(full_eval_dataset)), config['data']['eval_samples'])
-    eval_dataset = Subset(full_eval_dataset, eval_indices)
-    del full_eval_dataset
-    
-    # Setup data collator
-    data_collator = DataCollatorForLanguageModeling(
-        tokenizer=tokenizer,
-        mlm=False,
-    )
-    
-    # Create model
-    model = create_model(config, tokenizer)
-    print(f'Model parameters = {model.num_parameters()}')
-    
-    # Setup training arguments
-    output_dir = Path(config['logging']['output_dir']) / config['model']['name']
-    accumulation_steps = config['training']['gradient_accumulation_steps']
-    per_device_bsz = config['training']['batch_size'] // accumulation_steps
-    
-    training_args = TrainingArguments(
-        output_dir=output_dir,
-        overwrite_output_dir=True,
-        save_strategy="epoch",
-        evaluation_strategy="epoch",
-        num_train_epochs=config['training']['num_epochs'],
-        gradient_accumulation_steps=accumulation_steps,
-        per_device_train_batch_size=per_device_bsz,
-        save_total_limit=1,
-        warmup_steps=config['training']['warmup_steps'],
-        lr_scheduler_type="cosine",
-        learning_rate=float(config['training']['lr']),
-        logging_steps=20,
-        fp16=config['training']['fp16'],
-        load_best_model_at_end=True,
-        metric_for_best_model="eval_loss",
-        torch_compile=config['training'].get('torch_compile', False),
-    )
-    
-    # Create trainer
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        data_collator=data_collator,
-        train_dataset=train_dataset,
-        eval_dataset=eval_dataset,
-    )
-    
-    # Check GPU availability
-    check_gpu_availability()
-    
-    # Train and save
     print(f"Trainer is using device: {trainer.args.device}")
     trainer.train()
     trainer.save_model(output_dir)
     tokenizer.save_pretrained(output_dir)
-
-
-if __name__ == "__main__":
-    main()
